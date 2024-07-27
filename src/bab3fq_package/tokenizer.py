28da82bb035d6575c97ebcf3690aca52e51f97e8
Brian <brianblancato@gmail.com>
2024-07-27 00:03:04 -0400

    Comment edits

--- src/bab3fq_package/tokenizer.py
+++ src/bab3fq_package/tokenizer.py
@@ -38,7 +38,7 @@ def clean_text(text):
 
 def tokenize(text):
     '''
-    Takes a string and reutrns a python list where each item is a word in the file.
+    Takes a string and returns a python list where each item is a word in the file.
     
     Args:
         text: An input string to be tokenized.
